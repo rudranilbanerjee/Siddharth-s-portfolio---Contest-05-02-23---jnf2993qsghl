4c2773cb1987a37b90fca76052e31eea6725e144
Rudranil Banerjee <105772744+rudranilbanerjee@users.noreply.github.com>
2023-02-05 11:25:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
-import {BrowserRouter} from 'react-router-dom';
+
+
 
 ReactDOM.render(
     <BrowserRouter>
-       <App />
+        <App />
     </BrowserRouter>
-   ,
-  document.getElementById("root"));
+    ,
+    document.getElementById("root"));
